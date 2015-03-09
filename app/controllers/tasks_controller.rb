b54b91a9f2ab13c78cf567f2198dc2fa2df47dd9
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-03-09 15:27:31 -0600

    fix project task link error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,128 +1,62 @@
 class TasksController < ApplicationController
-
-  before_action :authenticate
-
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate
 
   def index
     @project = Project.find(params[:project_id])
-    @tasks = Task.all
+    # @tasks = Task.all
   end
 
-    def show
-      @project = Project.find(params[:project_id])
-      @task = @project.tasks.find(params[:id])
-    end
-  
+  def show
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+  end
+
   def new
     @project = Project.find(params[:project_id])
     @task = Task.new
   end
 
-  def edit
-    @project = Project.find(params[:project_id])
-    @task.project_id = @project.id
-  end
-
   def create
-    @project  = Project.find(params[:project_id])
+    @project = Project.find(params[:project_id])
     @task = Task.new(task_params)
     @task.project_id = @project.id
-      if @task.save
-        redirect_to project_tasks_path(@project), notice: 'Task was successfully created.'
-      else
-        render :new
-      end
+
+    if @task.save
+      redirect_to project_tasks_path(@project), notice: "Task was successfully created!"
+    else
+      render :new
     end
+  end
+
+  def edit
+    @project = Project.find(params[:project_id])
+    @task.project_id = params[:project_id]
+  end
 
   def update
-    @project  = Project.find(params[:project_id])
-      if @task.update(task_params)
-        redirect_to project_path(@project), notice: 'Task was successfully updated.'
-      else
-        render :edit
-      end
+    @project = Project.find(params[:project_id])
+    @task.project_id = params[:project_id]
+    if @task.update(task_params)
+      redirect_to project_path(@task.project_id), notice: "Task was successfully updated!"
+    else
+      render :edit
     end
-
+  end
 
   def destroy
-    @project  = Project.find(params[:project_id])
-    @task = Task.find(params[:id])
+    @project = Project.find(params[:project_id])
+    @task.project_id = params[:project_id]
     @task.destroy
-      redirect_to project_tasks_path(@project), notice: 'Task was successfully destroyed.'
-    end
-
+    redirect_to project_tasks_path(@project), notice: "Task was successfully destroyed!"
+  end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_task
     @task = Task.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def task_params
     params.require(:task).permit(:description, :due, :completed, :project_id)
   end
 end
-# class TasksController < ApplicationController
-#   before_action :set_task, only: [:show, :edit, :update, :destroy]
-#   before_action :authenticate
-#
-#   def index
-#     @project = Project.find(params[:project_id])
-#   end
-#
-#   def show
-#     @project = Project.find(params[:project_id])
-#     @task = @project.tasks.find(params[:id])
-#   end
-#
-#   def new
-#     @project = Project.find(params[:project_id])
-#     @task = Task.new
-#   end
-#
-#   def create
-#     @project = Project.find(params[:project_id])
-#     @task = Task.new(task_params)
-#     @task.project_id = @project.id
-#
-#     if @task.save
-#       redirect_to project_tasks_path(@project), notice: "Task was successfully created!"
-#     else
-#       render :new
-#     end
-#   end
-#
-#   def edit
-#     @project = Project.find(params[:project_id])
-#     @task.project_id = params[:project_id]
-#   end
-#
-#   def update
-#     @project = Project.find(params[:project_id])
-#     @task.project_id = params[:project_id]
-#     if @task.update(task_params)
-#       redirect_to project_path(@task.project_id), notice: "Task was successfully updated!"
-#     else
-#       render :edit
-#     end
-#   end
-#
-#   def destroy
-#     @project = Project.find(params[:project_id])
-#     @task.project_id = params[:project_id]
-#     @task.destroy
-#     redirect_to project_tasks_path(@project), notice: "One less thing to do!"
-#   end
-#
-#   private
-#   def set_task
-#     @task = Task.find(params[:id])
-#   end
-#
-#   def task_params
-#     params.require(:task).permit(:description, :duedate, :completed, :project_id)
-#   end
-# end
