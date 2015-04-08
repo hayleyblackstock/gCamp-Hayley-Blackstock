52dd93029c619fbeea863bf86ffeb3575f900967
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-04-08 15:15:53 -0600

    add error message and validation for user to be added to project once

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,7 +3,8 @@ class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  enum role: [:member, :owner]
+  enum role: {member: 0, owner: 1}
 
+  validates :user_id, presence: true, uniqueness: {scope: :project_id, message: "has already been added to this project."}
 
 end
