679a56cb730a4e730f525c5e525afbd2c547869e
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-04-15 15:04:24 -0600

    add stats to about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @users = User.all
+    @comments = Comment.all
+    @memberships = Membership.all
   end
 end
