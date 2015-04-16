7439a25b11f7c92d77b8dd2822a00530b5a832b6
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-04-16 10:43:59 -0600

    change signin users path

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -8,7 +8,7 @@ def create
   @user = User.find_by_email(params[:email])
   if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
-    redirect_to root_path, notice: 'Welcome back to gCamp!'
+    redirect_to projects_path, notice: 'Welcome back to gCamp!'
   else
     @sign_in_error = "Username / password combination is invalid"
     render :new
